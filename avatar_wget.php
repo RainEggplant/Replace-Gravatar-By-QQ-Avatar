e274d9173af5d7bae69eca7eb4340c45efb6de64
RainEggplant <admin@raineggplant.com>
2018-05-03 09:48:46 +0800

    Update avatar_wget.php

--- avatar_wget.php
+++ avatar_wget.php
@@ -73,11 +73,7 @@ function my_get_avatar($avatar, $id_or_email, $size) {
 	// Unfortunately I don't know what encrypt method the QQ avatar interface accepts.
 	// So to protect the author's privacy, I have to cache the avatars.
 	$wp_url = get_bloginfo('wpurl');
-	// Caching by creating and executing another php file to avert waiting.
-	// Use random filename to avoid interference.
-	$sh_filename = 'avatar_downloader-' . rand() . '.php';
-	$sh_file = fopen(ABSPATH . 'avatar/' . $sh_filename, "w");
-	fwrite($sh_file, "<?php\n");
+	// Caching.
 	for ( $i = 0; $i < $url_count; ++$i) {
 		$file_path = ABSPATH . 'avatar/' . $hashkey . '-' . $avatar_size[$i] . '.jpg';
 		// 1209600s = 14d, the avatars will be cached for 2 weeks. You can change the period.
@@ -85,18 +81,11 @@ function my_get_avatar($avatar, $id_or_email, $size) {
 		if ( !is_file($file_path) || (time() - filemtime($file_path) ) > $lifetime) { 
 			// If the file doesn't exist or it has been out of date, then update it.
 			// It's necessary to use "-N --no-use-server-timestamps".
-			$txt = "exec(\"wget -N --no-use-server-timestamps -O '" . $file_path . "' '" . $avatar_url[$i] . "'\");\n";			
-			fwrite($sh_file, $txt); 
+			exec("wget -N --no-use-server-timestamps -O '" . $file_path . "' '" . $avatar_url[$i] . "'  > /dev/null &");						
 		}
 		else $avatar = str_replace($original_avatar_url[0][$i], $wp_url . '/avatar/' . $hashkey . '-' . $avatar_size[$i] . '.jpg', $avatar);
 		if ( filesize($file_path) < 500 ) copy($wp_url . '/avatar/default.jpg', $file_path);
 	}
-	// Make the temporary php file delete itself after being executed.
-	fwrite($sh_file, "\$file = '" . ABSPATH . 'avatar/' . $sh_filename . "';\n");
-	fwrite($sh_file, "if ( file_exists(\$file) ) @unlink(\$file);\n");
-	fwrite($sh_file, "?>\n");
-	fclose($sh_file);
-	exec("php '" . ABSPATH . "avatar/" . $sh_filename . "' > /dev/null &");
 	return $avatar;	
 }
 add_filter( 'get_avatar', 'my_get_avatar', 10, 3);
